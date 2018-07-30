8a45dfca98e60b76a78ace8255ef3855bfb90cdc
osmel calderón bernal <osmel.calderon@gmail.com>
2018-07-30 10:10:15 -0500

    actualizando administrador

--- mapa/app/config/routes.php
+++ mapa/app/config/routes.php
@@ -53,6 +53,43 @@ $route['cargar_dependencia']   	= 'main/cargar_dependencia';
 
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////Administracion//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$route['admin']							= 'admin/index';
+$route['login']							= 'admin/login';
+
+
+$route['usuarios']						= 'admin/listado_usuarios'; 
+$route['procesando_usuarios']			= 'admin/procesando_usuarios';
+
+
+	/* necesita server de correo, para que notifique quien se da de alta*/
+$route['nuevo_usuario']                 = 'admin/nuevo_usuario';
+$route['validar_nuevo_usuario']         = 'admin/validar_nuevo_usuario';
+
+$route['actualizar_perfil']		         = 'admin/actualizar_perfil';
+$route['editar_usuario/(:any)']			= 'admin/actualizar_perfil/$1';
+$route['validacion_edicion_usuario']    = 'admin/validacion_edicion_usuario';
+
+$route['eliminar_usuario/(:any)']		= 'admin/eliminar_usuario/$1';
+$route['validar_eliminar_usuario']    = 'admin/validar_eliminar_usuario';
+
+
+$route['salir']							= 'admin/logout';
+
+$route['validar_login']					= 'admin/validar_login';
+
+
+//recuperar contraseña /* necesita server de correo*/
+$route['recuperar_contrasena']			= 'admin/recuperar_contrasena';
+$route['validar_recuperar_password']	= 'admin/validar_recuperar_password';
+
+//historicos de accesos
+$route['historico_accesos']                 = 'admin/historico_accesos';
+$route['procesando_historico_accesos']      = 'admin/procesando_historico_accesos';
+
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
